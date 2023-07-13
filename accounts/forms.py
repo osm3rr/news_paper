6d93341921c48925c9fc43f83a4a7186041a18e5
osm3rr <osm3rr@gmail.com>
2023-07-13 06:31:13 -0400

    email added in sign up

--- accounts/forms.py
+++ accounts/forms.py
@@ -4,8 +4,16 @@ from .models import CustomUser
 class CustomUserCreationForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
         model = CustomUser
-        fields = UserCreationForm.Meta.fields + ("age",)
+        fields = (
+            "username",
+            "email",
+            "age",
+            )
 
 class CustomUserChangeForm(UserChangeForm):
     class Meta:
-        fields = UserChangeForm.Meta.fields
+        fields = (
+            "username",
+            "email",
+            "age",
+            )
